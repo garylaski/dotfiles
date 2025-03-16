394541f7de10a5e7ec5b8bf32ae606096d85761a
gary <28818063+garylaski@users.noreply.github.com>
2025-03-16 10:25:20 -0400

    optional_packages.sh -> packages.sh

--- .local/bin/packages.sh
+++ .local/bin/packages.sh
@@ -7,9 +7,9 @@ base_packages() {
     else
         video="mesa-dri mesa-vaapi mesa-vdpau mesa-vulkan-radeon"
     fi
-    de="sway seatd dbus nodejs neovim 7zip unzip curl wget gzip ghostty firefox zsh jq fzf"
+    de="sway seatd dbus nodejs neovim 7zip unzip curl wget gzip ghostty firefox zsh jq fzf htop zenity"
     fonts="noto-fonts-ttf noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf-extra"
-    dev="git base-devel gnupg"
+    dev="git base-devel gnupg python3-pip"
     audio="pipewire alsa-pipewire"
     xdg="xdg-user-dirs xdg-utils xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk"
     printf " $base $video $de $fonts $dev $audio $xdg "
@@ -18,16 +18,43 @@ base_packages() {
 optional_packages() {
     xdg_ninja="glow"
     rust_dev="cargo openssl-devel"
+    java="openjdk21 openjdk21-jre"
     flatpak="flatpak"
     screenshot="grimshot"
     image_edit="gimp"
     media_utils="picard ffmpeg qbittorrent mpv"
-    printf " $xdg_ninja $rust_dev $flatpak $screenshot $image_edit $media_utils "
+    printf " $java $xdg_ninja $rust_dev $flatpak $screenshot $image_edit $media_utils "
 }
 
 flatpak_packages() {
     obsidian="md.obsidian.Obsidian" 
-    bottles="com.usebottles.bottles org.freedesktop.Platform.VulkanLayer.gamescope//24.08 org.freedesktop.Platform.VulkanLayer.vkBasalt//stable "
+    bottles="com.usebottles.bottles"
     steam="com.valvesoftware.Steam"
     printf " $obsidian $bottles $steam "
 }
+
+sudo xbps-install -Syu $(optional_packages) $(base_packages)
+sudo xbps-install -Syu
+
+flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak --user install flathub $(flatpak_packages)
+flatpak --user update
+
+# Check installed packages to make sure only the necessary packages are installed
+installed_packages=$(xbps-query -m | sed 's/-[0-9+_.].*//')
+all="$(base_packages; optional_packages)"
+for package in $installed_packages; do
+    case $all in
+        *" $package "*) ;;
+        *) echo "WARNING: $package found, yet not recorded.";; 
+    esac
+done
+
+flatpak_installed=$(flatpak list --app --columns=application)
+all="$(flatpak_packages)"
+for package in $flatpak_installed; do
+    case $all in
+        *" $package "*) ;;
+        *) echo "WARNING: $package found, yet not recorded.";; 
+    esac
+done
